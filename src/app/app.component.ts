cc2cf8f526534581dfd3cbf5cae8c25ecf57376f
Miguel Angel Jimenez <miguejm@outlook.es>
2023-02-02 22:08:28 -0600

    template and styles properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+//   template: `<h1>Todo bien, todo correcto y yo que me alegro.</h1>`,
   styleUrls: ['./app.component.sass']
+//   styles: [``]
 })
 export class AppComponent {
   title = 'backlog-frontend';
