7044183e9710a7acea9c163105c7229d554a983d
Miguel Angel Jimenez <miguejm@outlook.es>
2023-02-03 12:58:51 -0600

    *ngFor

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from '../interfaces/user';
 
 @Component({
   selector: 'app-form',
@@ -7,6 +8,48 @@ import { Component } from '@angular/core';
 })
 export class FormComponent {
   show_alert: boolean = false;
+  users: User[] = [
+    {
+      firstname: 'Miguel',
+      lastname: 'Angel',
+      username: 'migueajm',
+      email: 'miguelj@infoexpo.com',
+      password: 'this passsword is secret',
+      active: true,
+    },
+    {
+      firstname: 'Sebastian',
+      lastname: 'Angel',
+      username: 'sebasjm',
+      email: 'miguelj@infoexpo.com',
+      password: 'this passsword is secret',
+      active: true,
+    },
+    {
+      firstname: 'Christopher',
+      lastname: 'Angel',
+      username: 'christojm',
+      email: 'miguelj@infoexpo.com',
+      password: 'this passsword is secret',
+      active: true,
+    },
+    {
+      firstname: 'Jesus',
+      lastname: 'Angel',
+      username: 'chuchojm',
+      email: 'miguelj@infoexpo.com',
+      password: 'this passsword is secret',
+      active: true,
+    },
+    {
+      firstname: 'Hector',
+      lastname: 'Angel',
+      username: 'chumajm',
+      email: 'miguelj@infoexpo.com',
+      password: 'this passsword is secret',
+      active: true,
+    },
+  ];
   showInConsole(username: string) {
     this.show_alert = true;
     console.log({ username });
