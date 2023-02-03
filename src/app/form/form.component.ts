e78a54817e510bd53333483087847d1bbc615009
Miguel Angel Jimenez <miguejm@outlook.es>
2023-02-03 12:31:38 -0600

    directiva *ngIf/else

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -6,7 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./form.component.sass'],
 })
 export class FormComponent {
+  show_alert: boolean = false;
   showInConsole(username: string) {
+    this.show_alert = true;
     console.log({ username });
   }
 }
