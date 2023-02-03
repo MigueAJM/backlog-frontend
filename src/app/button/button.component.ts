4eee15e0d18e53d012b49f82c089003001010a44
Miguel Angel Jimenez <miguejm@outlook.es>
2023-02-03 11:43:28 -0600

    property binding o enlaces de propiedades

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -3,9 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.sass'],
+  styleUrls: ['./button.component.sass']
 })
 export class ButtonComponent {
   text_color: string = 'text-dark';
   button_disabled: boolean = false;
+  src: string = 'https://images.pexels.com/photos/5945960/pexels-photo-5945960.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load'
 }
