7b3e0b1be28690962ae34f72190feb8d191ac61b
Miguel Angel Jimenez <miguejm@outlook.es>
2023-02-05 09:58:35 -0600

    formBuilder

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
 import { User } from '../interfaces/user';
 
 @Component({
@@ -8,19 +13,28 @@ import { User } from '../interfaces/user';
   styleUrls: ['./reactive-form.component.sass'],
 })
 export class ReactiveFormComponent {
+  constructor(private fb: FormBuilder) {}
   get email() {
     return this.form_user.get('email');
   }
   get password() {
     return this.form_user.get('password') as FormControl;
   }
-  form_user = new FormGroup({
+  /* form_user = new FormGroup({
     firstname: new FormControl('', Validators.required),
     lastname: new FormControl('', [Validators.required]),
     username: new FormControl('', Validators.required),
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', Validators.required),
     active: new FormControl(false),
+  }); */
+  form_user = this.fb.group({
+    name: ['', Validators.required],
+    lastname: ['', [Validators.required]],
+    username: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+    active: [false],
   });
   process() {
     console.info({ form_user: this.form_user.value });
