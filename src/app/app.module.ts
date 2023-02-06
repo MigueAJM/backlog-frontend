4f96fa732e30503061e758cdaeafe78ee3d69043
Miguel Angel Jimenez <miguejm@outlook.es>
2023-02-06 15:39:55 -0600

    RouterModule and Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,40 @@ import { ChildrenComponent } from './children/children.component';
 
 import { ListMessagesComponent } from './list-messages/list-messages.component';
 import { AddMessagesComponent } from './add-messages/add-messages.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AboutComponent } from './components/about/about.component';
 
+import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
+// main/home
+// contact
+// about
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,8 +58,17 @@ import { AddMessagesComponent } from './add-messages/add-messages.component';
     ChildrenComponent,
     ListMessagesComponent,
     AddMessagesComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutComponent,
+    NotFoundComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
